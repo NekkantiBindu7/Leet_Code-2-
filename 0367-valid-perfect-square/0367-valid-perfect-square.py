d4e383d31be943869e441bc8e093eef8900b730c
Nekkanti Bindu <139706931+NekkantiBindu7@users.noreply.github.com>
2024-02-11 04:07:40 +0530

    Time: 38 ms (48.21%), Space: 16.6 MB (53.38%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.py
@@ -0,0 +1,6 @@
+
+class Solution:
+  def isPerfectSquare(self, num: int) -> bool:
+    l = bisect.bisect_left(range(num), num,
+                           key=lambda m: m * m)
+    return l**2 == num
