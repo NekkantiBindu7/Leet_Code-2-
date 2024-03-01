41bfc6777778bc636466c34cbffacba1b4799a84
Nekkanti Bindu <139706931+NekkantiBindu7@users.noreply.github.com>
2024-03-01 21:06:24 +0530

    Update 0070-climbing-stairs.py

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,10 +1,12 @@
-
 class Solution:
   def climbStairs(self, n: int) -> int:
-    # dp[i] := the number of ways to climb to the i-th stair
-    dp = [1, 1] + [0] * (n - 1)
-
-    for i in range(2, n + 1):
-      dp[i] = dp[i - 1] + dp[i - 2]
-
-    return dp[n]
+    if n<=2:
+        return n
+    prev1 = 1
+    prev2 = 2
+    current = 0
+    for i in range(2, n):
+        current = prev1 + prev2
+        prev1 = prev2
+        prev2 = current
+    return current
